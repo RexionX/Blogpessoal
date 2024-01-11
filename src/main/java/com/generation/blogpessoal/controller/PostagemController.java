81b90a3f35ec2b9dc665f86cc491fab611a8ff27
Jhonnattan Crescencio <jhonnattan.cres10@gmail.com>
2024-01-11 13:38:15 -0300

    Blogpessoal ORMs

---
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,39 @@
+package com.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders="*")//informa toda e qualquer origem pode acessar essa rota 
+public class PostagemController {
+	
+	//injeção de dependencias 
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	//acessado verbo get
+	public ResponseEntity<List<Postagem>> getAll(){
+		//respostas no fomato http
+		
+		return ResponseEntity.ok(postagemRepository.findAll());
+		//find all == select * from nomedatabela
+		//codigo de retorno -- corpo de retorno 
+	}
+	
+	@GetMapping
+	public ResponseEntity<String> exemplo(){
+		String ola ="Olá mundo";
+		return ResponseEntity.ok(ola);
+	}
+
+}
