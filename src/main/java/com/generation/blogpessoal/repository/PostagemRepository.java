f769a306624470b1c02c49d47555200134649db5
Jhonnattan Crescencio <jhonnattan.cres10@gmail.com>
2024-01-12 17:44:06 -0300

    Atividade Tema

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,10 +1,15 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
 //JPA da o poder da repository cominicar-se com o meu banco de dados 
 public interface PostagemRepository extends JpaRepository <Postagem, Long> {
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("título")String titulo);
 
 }
